2088e2937415b7e2c0b564534ef6b4e4e3c8a964
HsiungWei <Hsiung,wei0812@gmail.com>
2019-03-07 14:00:08 +0800

    Initial Commit

---
+++ Mr.million/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mr.million
+//
+//  Created by HsiungWei on 2019/3/7.
+//  Copyright © 2019 熊偉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
